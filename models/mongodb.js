a9f993b2764fc2b594e6464b927b1b66da54252b
Chrysna ardy <60012187+chrysnaardy123@users.noreply.github.com>
2020-03-02 14:16:43 +0700

    Update mongodb.js

--- models/mongodb.js
+++ models/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
  
-mongoose.connect('mongodb://localhost:27017/ChrysnaCourseDB', {useNewUrlParser: true,useUnifiedTopology: true}, (err) => {
+mongoose.connect('mongodb://localhost:27017/', {useNewUrlParser: true,useUnifiedTopology: true}, (err) => {
 if (!err) {
 console.log('MongoDB connected')
 }
